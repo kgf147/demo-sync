451261ab8688598dd0e5fc88d88c8cb272543e7d
kgf147 <114506078+kgf147@users.noreply.github.com>
2023-06-25 16:39:39 +0530

    Update SynADOtoGitHub.ps1

--- SynADOtoGitHub.ps1
+++ SynADOtoGitHub.ps1
@@ -1,68 +1,78 @@
 param(
-     [Parameter()]
-     [string]$GitHubDestinationPAT,
+    [Parameter()]
+    [string]$GitHubDestinationPAT,
  
-     [Parameter()]
-     [string]$ADOSourcePAT,
+    [Parameter()]
+    [string]$ADOSourcePAT,
      
-     [Parameter()]
-     [string]$AzureRepoName,
+    [Parameter()]
+    [string]$AzureRepoName,
      
-     [Parameter()]
-     [string]$ADOCloneURL,
+    [Parameter()]
+    [string]$ADOCloneURL,
      
-     [Parameter()]
-     [string]$GitHubCloneURL
- )
+    [Parameter()]
+    [string]$GitHubCloneURL
+)
 
 # Write your PowerShell commands here.
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
-Write-Host ' reflect Azure Devops repo changes to GitHub repo'
+Write-Host ' Reflect Azure DevOps repo changes to GitHub repo'
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
-#$AzureRepoName = "CloudQuickLabsADO"
-#$ADOCloneURL = "dev.azure.com/CloudQuickLabs/CloudQuickLabsADO/_git/CloudQuickLabsADO"
-#$GitHubCloneURL = "github.com/RekhuGopal/CloudQuickLabsADO.git"
-$stageDir = pwd | Split-Path
-Write-Host "stage Dir is : $stageDir"
-$githubDir = $stageDir +"\"+"gitHub"
-Write-Host "github Dir : $githubDir"
-$destination = $githubDir+"\"+ $AzureRepoName+".git"
+
+$stageDir = Get-Location
+Write-Host "stage Dir is: $stageDir"
+
+$githubDir = Join-Path $stageDir "gitHub"
+Write-Host "github Dir: $githubDir"
+
+$destination = Join-Path $githubDir ($AzureRepoName + ".git")
 Write-Host "destination: $destination"
-#Please make sure, you remove https from azure-repo-clone-url
-$sourceURL = "https://$($ADOSourcePAT)"+"@"+"$($ADOCloneURL)"
-write-host "source URL : $sourceURL"
-#Please make sure, you remove https from github-repo-clone-url
-$destURL = "https://" + $($GitHubDestinationPAT) +"@"+"$($GitHubCloneURL)"
-write-host "dest URL : $destURL"
-#Check if the parent directory exists and delete
-if((Test-Path -path $githubDir))
-{
-  Remove-Item -Path $githubDir -Recurse -force
+
+# Please make sure to remove 'https://' from the Azure Repo clone URL
+$sourceURL = "https://$($ADOSourcePAT)@$($ADOCloneURL)"
+Write-Host "source URL: $sourceURL"
+
+# Please make sure to remove 'https://' from the GitHub Repo clone URL
+$destURL = "https://$($GitHubDestinationPAT)@$($GitHubCloneURL)"
+Write-Host "dest URL: $destURL"
+
+# Check if the parent directory exists and delete
+if (Test-Path -Path $githubDir) {
+    Remove-Item -Path $githubDir -Recurse -Force
 }
-if(!(Test-Path -path $githubDir))
-{
-  New-Item -ItemType directory -Path $githubDir
-  Set-Location $githubDir
-  git clone --mirror $sourceURL
+
+if (!(Test-Path -Path $githubDir)) {
+    New-Item -ItemType Directory -Path $githubDir
+    Set-Location $githubDir
+    git clone --mirror $sourceURL
+} else {
+    Write-Host "The given folder path $githubDir already exists"
 }
-else
-{
-  Write-Host "The given folder path $githubDir already exists";
+
+if (Test-Path -Path $destination) {
+    Set-Location $destination
+    Write-Output '*****Git removing remote secondary****'
+    git remote rm secondary
+
+    Write-Output '*****Git remote add****'
+    git remote add --mirror=fetch secondary $destURL
+
+    Write-Output '*****Git fetch origin****'
+    git fetch $sourceURL
+
+    Write-Output '*****Git push secondary****'
+    git push secondary --all -f
+
+    Write-Output '**Azure DevOps repo synced with GitHub repo**'
+} else {
+    Write-Host "The destination path $destination does not exist"
 }
-Set-Location $destination
-Write-Output '*****Git removing remote secondary****'
-git remote rm secondary
-Write-Output '*****Git remote add****'
-git remote add --mirror=fetch secondary $destURL
-Write-Output '*****Git fetch origin****'
-git fetch $sourceURL
-Write-Output '*****Git push secondary****'
-#git remote set-url origin $destURLSetURL
-git push secondary  --all -f
-Write-Output '**Azure Devops repo synced with Github repo**'
+
 Set-Location $stageDir
-if((Test-Path -path $githubDir))
-{
- Remove-Item -Path $githubDir -Recurse -force
+
+if (Test-Path -Path $githubDir) {
+    Remove-Item -Path $githubDir -Recurse -Force
 }
-write-host "Job completed"
+
+Write-Host "Job completed"
